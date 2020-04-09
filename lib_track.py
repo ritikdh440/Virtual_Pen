aa111ae93db8186475fff1d4a50e9bd0fb3e9b45
Avishrant Sharma <avishrants@gmail.com>
2020-04-09 12:29:34 +0530

    Sobel Filteration

--- lib_track.py
+++ lib_track.py
@@ -7,10 +7,14 @@ class tracker(object):
 
     def preprocess(self):
         tracker.pimg = cv2.cvtColor(self.img,cv2.COLOR_BGR2GRAY)
-        #tracker.pimg = cv2.GaussianBlur(tracker.pimg, (5, 5), 0)
-        ref = np.min(tracker.pimg)
-        min = ref+10 if ref < 50 else 0
-        #ret,tracker.pimg = cv2.threshold(tracker.pimg,min,255,cv2.THRESH_BINARY)
-        #return tracker.pimg
-
+        tracker.pimg = cv2.GaussianBlur(tracker.pimg, (5, 5), 0)
+        sobel = self.sobel()
         return self.img
+
+     def sobel(self):
+        sobelx = cv2.Sobel(tracker.pimg,cv2.CV_16S,1,0)
+        sobely = cv2.Sobel(tracker.pimg,cv2.CV_16S,0,1)
+        sobel = np.uint8(np.hypot(sobelx,sobely))
+        mean = np.mean(sobel)
+        sobel[sobel <= mean] = 0
+        return sobel
